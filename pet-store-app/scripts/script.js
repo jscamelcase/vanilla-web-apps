582d851ebd5e935e68c0cd15734332b90ec9278f
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-01-18 21:42:14 +0000

    Updated Event Listeners

--- pet-store-app/scripts/script.js
+++ pet-store-app/scripts/script.js
@@ -1,4 +1,4 @@
-//Mobile touchstart callback function
+// Mobile touchstart callback function
 const startTouchHover = function (event) {
   if (event.target.id === "start") {
     event.target.classList.add("touch-hover-effect");
@@ -9,7 +9,21 @@ const endTouchHover = function (event) {
   event.target.classList.remove("touch-hover-effect");
 };
 
-// Mobile solution to no hover on button touches
+// "click" event handler general callback function
+const handleClicks = function (event) {
+  if (event.target.id === "start") {
+    console.log("hello");
+    document.querySelector(".intro-title-section").style.display = "none";
+    // }
+  }
+  console.log("hello");
+};
+
+// Mobile event listener solution to no hover on button touches
 document.addEventListener("touchstart", startTouchHover);
 
 document.addEventListener("touchend", endTouchHover);
+
+// "click" event handler
+
+document.getElementById("start").addEventListener("click", handleClicks);
