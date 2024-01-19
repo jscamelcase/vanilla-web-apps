2a8c6d2fa237d03ca999d73978d02aa45b972d67
Carl Paraskevas <carl.paraskevas@gmail.com>
2024-01-19 18:52:15 +0000

    added offer banner, data, and data rendering

---
+++ pet-store-app/data/data.js
@@ -0,0 +1,61 @@
+const menuList = [
+  {
+    name: "Organic Chews",
+    price: 15,
+    quanity: "10 sticks",
+    shortDescription:
+      "A pack of 10 dog chews made from all organic ingredients made fresh from our kitchen. They will keep your dogs teeth healthy with a sparkle.",
+    ingredients: [
+      "beef trimmings",
+      "beef hearts",
+      "pork rind",
+      "dried chicken",
+      "wheat",
+      "corn",
+      "salt",
+      "sandthorn",
+      "seaweed",
+      "spirulina",
+    ],
+    productId: "dc-1",
+    image: "./images/chew.png",
+  },
+  {
+    name: "Beef & Bacon Pat&#233",
+    price: 30,
+    quantity: "150g",
+    shortDescription:
+      "Spoil your dog with this truly luxurious treat. Made from organic British beef, you won't find a tastier source of protien.",
+    ingredients: [
+      "Organic chickpeas",
+      "water",
+      "organic beef",
+      "organic olive oil",
+      "organic ginger",
+      "organic thyme",
+      "tocopherol of natural origin",
+    ],
+    productId: "pt-1",
+    image: "./images/pate.png",
+  },
+  {
+    name: "Bedtime Biscuits",
+    price: 25.7,
+    shortDescription:
+      "This delightful oven baked biscuts contain a mixture of natural ingredients to help soothe your dog for bedtime and provide good bacteria for gut health.",
+    ingredients: [
+      "Oranic Ligh Rye Flour",
+      "Organic Oat Flour",
+      "Organic Oats",
+      "Organic Yogurt",
+      "Prganic Sunflower Oil",
+      "Nutritional Yeast",
+      "Organic Honey",
+      "Organic Chamomile",
+    ],
+    productId: "bb-1",
+    image: "./images/biscut.png",
+  },
+];
+
+export { menuList };
--- pet-store-app/scripts/script.js
+++ pet-store-app/scripts/script.js
@@ -1,3 +1,6 @@
+//Import data
+import { menuList } from "../data/data.js";
+
 // Mobile touchstart callback function
 const startTouchHover = function (event) {
   if (event.target.id === "start") {
@@ -9,14 +12,74 @@ const endTouchHover = function (event) {
   event.target.classList.remove("touch-hover-effect");
 };
 
+const addQuantity = function (event) {
+  console.log("Button Working");
+};
+
+/* Generate Menu List */
+const generateMenu = function () {
+  // Grab hold of menu items container to place each menu item inside the container after loop construction
+  const menuPageItems = document.querySelector(
+    ".menu-page-order-items-container"
+  );
+  for (let item of menuList) {
+    /*Create a container on each round of the loop to place the different menu items in consideration of flexbox styling */
+    const menuItemContainer = document.createElement("div");
+    menuItemContainer.classList.add("menu-item");
+    /* Create individual components of each menu item */
+    const menuItemImage = document.createElement("img");
+    menuItemImage.classList.add("menu-img");
+    const menuItemDesContainer = document.createElement("div");
+    menuItemDesContainer.classList.add("menu-description-container");
+    const menuItemDescription = document.createElement("p");
+    menuItemDescription.classList.add("menu-description");
+    const menuItemIngredients = document.createElement("p");
+    menuItemIngredients.classList.add("menu-ingredients");
+    const menuItemPrice = document.createElement("p");
+    menuItemPrice.classList.add("menu-price");
+    const menuItemButtonImg = document.createElement("img");
+    menuItemButtonImg.classList.add("menu-btn-img");
+    menuItemButtonImg.src = "../images/addItem.svg";
+    menuItemButtonImg.addEventListener("click", addQuantity);
+    /* Unpack each property from each object in the data array to go into each menu item section*/
+    const {
+      name,
+      price,
+      quantity,
+      shortDescription,
+      ingredients,
+      productId,
+      image,
+    } = item;
+    /* Add respective values to each created element*/
+    menuItemImage.src = `${image}`;
+    menuItemDescription.textContent = `${shortDescription}`;
+    menuItemIngredients.textContent = `${ingredients.join(", ")}`;
+    menuItemPrice.textContent = `$${price}`;
+    menuItemButtonImg.dataset.item = `${productId}`;
+    /*compose each element together in the right order ahead of appending to the HTML menu section*/
+    menuItemDesContainer.append(
+      menuItemDescription,
+      menuItemIngredients,
+      menuItemPrice
+    );
+    menuItemContainer.append(
+      menuItemImage,
+      menuItemDesContainer,
+      menuItemButtonImg
+    );
+    // Append menu item container to menuPageItems for rendering
+    menuPageItems.appendChild(menuItemContainer);
+  }
+};
+
 // "click" event handler general callback function
 const handleClicks = function (event) {
   if (event.target.id === "start") {
     console.log("hello");
-    document.querySelector(".intro-title-section").style.display = "none";
-    // }
+    document.querySelector(".intro-container").style.display = "none";
+    generateMenu();
   }
-  console.log("hello");
 };
 
 // Mobile event listener solution to no hover on button touches
